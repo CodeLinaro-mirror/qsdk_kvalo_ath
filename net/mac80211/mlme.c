623bf56a9f0906d780f46ae110b47212ac3406ec
Jeff Johnson <jeff.johnson@oss.qualcomm.com>
2026-07-21 14:04:55 -0700

    wifi: mac80211: Fixup merge between wireless and wireless-next
    WAR for a divergence between wireless and wireless-next until
    the next time I recreate ath/main (at which time I'll add this
    to the wireless-next merge into wireless).
    
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -7276,7 +7276,7 @@ static void ieee80211_rx_mgmt_assoc_resp(struct ieee80211_sub_if_data *sdata,
 	else if (!assoc_data->s1g)
 		aid = le16_to_cpu(mgmt->u.assoc_resp.aid);
 	else if (status_code == WLAN_STATUS_SUCCESS)
-		goto abandon_assoc;
+		goto destroy_assoc_data;
 
 	/*
 	 * The 5 MSB of the AID field are reserved for a non-S1G STA. For
